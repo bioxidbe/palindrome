976c28651fcb3a2df32380c5570528c6dd361db3
bioxid <bioxid@hotmail.com>
2019-01-23 15:59:34 +0100

    commit branche

--- BllPalindrome/PalindromeManagement.cs
+++ BllPalindrome/PalindromeManagement.cs
@@ -15,34 +15,58 @@ namespace BllPalindrome
             FileTextReaderUtility myReader = new FileTextReaderUtility();
             List<string> redLines = myReader.ReadAllLines(pFileName);
             List<string> cleanedList = new List<string>();
-
             foreach (string lines in redLines)
             {
+                if (string.IsNullOrWhiteSpace(lines) == false)
+                {
+                    string[] splitted = lines.Split(',');
+                    cleanedList.Add(NoBrackets(splitted[0].Trim()));
+                }
                 //example line contiens " aa, une type de lave"
                 //cree algorythme, a la fin ne rreste que "aa"
-                string[] splitted = lines.Split(',');
-                cleanedList.Add(splitted[0].Trim());
+
             }
             return cleanedList;
+        }
 
-            //List<string> resultClean = new List<string>();
-            //foreach (string regLine in redLines)
-            //{
-            //    string pattern;
-            //    pattern = ".+?(?=,)";
-            //    Regex rgx = new Regex(pattern);
+        public string NoBrackets(string input)
+        {
+            StringBuilder noBrackets = new StringBuilder();
+            int level = 0;
+            string output = string.Empty;
+            foreach (var c in input)
+            {
+                if (c == '(')
+                {
+                    level++;
+                }
+                if (level == 0)
+                {
+                    noBrackets.Append(c);
+                }
+                if (c == ')' && level > 0)
+                {
+                    level--;
+                }
 
-            //    MatchCollection matches = rgx.Matches(regLine);
-
-            //    if (rgx.IsMatch(regLine))
-            //    {
-            //        foreach (Match match in matches)
-            //            resultClean.Add("" + match.Value);
-            //    }
-            //}
-            //return resultClean;
-
-
+            }
+            return noBrackets.ToString();
         }
     }
 }
+//List<string> resultClean = new List<string>();
+//foreach (string regLine in redLines)
+//{
+//    string pattern;
+//    pattern = ".+?(?=,)";
+//    Regex rgx = new Regex(pattern);
+
+//    MatchCollection matches = rgx.Matches(regLine);
+
+//    if (rgx.IsMatch(regLine))
+//    {
+//        foreach (Match match in matches)
+//            resultClean.Add("" + match.Value);
+//    }
+//}
+//return resultClean;
